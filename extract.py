7f2ddbfc8a15dec0237e5150d5292d8912974efb
nikumar1013 <nkumar@utexas.edu>
2020-03-19 12:52:34 -0500

    Improved design and flow of main.py

--- extract.py
+++ extract.py
@@ -1,5 +1,5 @@
 
-# Parses data to get a user's top 10 artists
+# Returns a list of the user's top 10 artists
 def get_top_artists(data):
     artists = []
     items = data['items']
@@ -8,7 +8,7 @@ def get_top_artists(data):
         artists.append(artist_name)
     return artists
 
-# Parses data to get a user's top 10 tracks
+# Returns a list of the user's top 10 tracks
 def get_top_tracks(data):
     tracks = []
     for item in data['items']:
@@ -17,3 +17,20 @@ def get_top_tracks(data):
         list_entry = track_name + " - " + artist_name
         tracks.append(list_entry)
     return tracks
+
+# Returns a python dictionary with the top 50 tracks grouped by the artist
+def get_top_tracks_by_artist(range):
+	dic = {}
+	if token:
+		sp = spotipy.Spotify(auth=token)
+		track_data = sp.current_user_top_tracks(limit=50, offset=0, time_range=range)
+		artist_data = sp.current_user_top_artists(limit=10, offset=0, time_range=range)
+		for item in artist_data['items']:
+			top_artist = item['name']
+			lst = []
+			for item in track_data['items']:
+				track_artist = item['artists'][0]['name']
+				if top_artist == track_artist:
+					lst.append(item['name'])
+			dic[top_artist] = lst
+	return dic
--- main.py
+++ main.py
@@ -19,25 +19,18 @@ base_url = "https://api.spotify.com/v1"
 redirect_uri = "http://127.0.0.1:8080/home"
 scope = "user-top-read"
 
-# Query parameters for get request
-query = {
+# Query parameters for authorization
+auth_query = {
     "response_type": "code",
     "redirect_uri": redirect_uri,
     "scope": scope,
-    "show_dialog": "true",
+    "show_dialog": "false",
     "client_id": client_id
 }
 
-@app.route("/")
-def index():
-    # Redirects the user to the Spotify login page (first thing that happens upon app launch)
-    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in query.items()])
-    authorization = "{}/?{}".format(auth_url, url_args)
-    return redirect(authorization)
 
-
-@app.route("/home")
-def display_top_data():
+# Returns a token needed to access the Spotify API
+def get_access_token():
     # Requests refresh and access tokens (POST)
     auth_token = request.args['code']
     code_payload = {
@@ -55,22 +48,56 @@ def display_top_data():
     refresh_token = response_data["refresh_token"]
     token_type = response_data["token_type"]
     expires_in = response_data["expires_in"]
+    return access_token
 
-    # Use the access token to access Spotify API
-    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
 
+def get_top_artist_data(authorization_header, time_range, limit, offset):
     # GET user top artist data
-    top_artist_endpoint = "{}/me/top/artists?time_range=long_term&limit=10&offset=0".format(base_url) # NEED TO FIGURE OUT HOW TO PARAMETERIZE THIS
+    top_artist_endpoint = "{}/me/top/artists?time_range={}&limit={}&offset={}".format(base_url, time_range, limit, offset) 
     top_artist_response = requests.get(top_artist_endpoint, headers=authorization_header)
     data = json.loads(top_artist_response.text)
     top_artist_data = extract.get_top_artists(data)
+    return top_artist_data
 
+
+def get_top_tracks_data(authorization_header, time_range, limit, offset):
     # GET user top artist data
-    top_tracks_endpoint = "{}/me/top/tracks?time_range=long_term&limit=10&offset=0".format(base_url) # NEED TO FIGURE OUT HOW TO PARAMETERIZE THIS
+    top_tracks_endpoint = "{}/me/top/tracks?time_range={}&limit={}&offset={}".format(base_url, time_range, limit, offset) 
     top_tracks_response = requests.get(top_tracks_endpoint, headers=authorization_header)
     data = json.loads(top_tracks_response.text)
     top_tracks_data = extract.get_top_tracks(data)
+    return top_tracks_data
+
+
+@app.route("/")
+def index():
+    # Redirects the user to the Spotify login page (first thing that happens upon app launch)
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query.items()])
+    authorization = "{}/?{}".format(auth_url, url_args)
+    return redirect(authorization)
+
+
+@app.route("/home")
+def display_top_data():
+    # Obtain an access token and use it to access the Spotify API
+    access_token = get_access_token()
+    authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+
+    # Retrieve the top artist data and top tracks data
+    top_artist_data = get_top_artist_data(authorization_header, 'long_term', '10', '0')
+    top_tracks_data = get_top_tracks_data(authorization_header, 'long_term', '10', '0')
+
+    # Render HTML with the desired data
     return render_template("index.html", artists=top_artist_data, tracks=top_tracks_data)
+
+
+# @app.route("/top-tracks-by-artist")
+# def display_top_tracks_by_artist():
+#     # Obtain an access token and use it to access the Spotify API
+#     access_token = get_access_token()
+#     authorization_header = {"Authorization": "Bearer {}".format(access_token)}
+
+#     return render_template("index.html")
 
 # Run the server
 if __name__ == "__main__":
