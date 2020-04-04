11a3fd4e7ddb70c19b997fb8a2efe770650f7089
nikumar1013 <nkumar@utexas.edu>
2020-04-03 22:33:43 -0500

    Text file no longer needed

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ from flask import Flask, request, redirect, g, render_template, Response, make_r
 from urllib.parse import quote
 from math import pi
 
-
 app = Flask(__name__)
     
 # API Keys
@@ -60,7 +59,6 @@ def generate_access_token():
     refresh_token = response_data["refresh_token"]
     token_type = response_data["token_type"]
     expires_in = response_data["expires_in"]
-
     return access_token
 
 
@@ -99,27 +97,6 @@ def get_recent_tracks_data(auth_header, limit):
     return recent_tracks_data
 
 
-# # GET an artist's related artists
-# def get_related_artists(auth_header, artist_id):
-#     if artist_id is None:
-#         return None
-#     endpoint = "{}/artists/{}/related-artists".format(base_url, artist_id)
-#     response = requests.get(endpoint, headers=auth_header)
-#     data = json.loads(response.text)
-#     related_artists = extract.related_artists(data)
-#     return related_artists
-
-
-# # GET the artist that a user has been listening to a lot recently (if there is one)
-# def get_frequent_artist(auth_header, artist_id):
-#     if artist_id is None:
-#         return None
-#     endpoint = "{}/artists/{}".format(base_url, artist_id)
-#     response = requests.get(endpoint, headers=auth_header)
-#     artist_name = json.loads(response.text)['name']
-#     return artist_name
-
-
 # GET the track ids from the user's recent listening history
 def get_recent_tracks_ids(auth_header, limit):
     endpoint = "{}/me/player/recently-played?type=track&limit={}".format(base_url, limit)
@@ -148,6 +125,70 @@ def get_artist_images(auth_header, artist_ids):
     return artist_images
 
 
+# GET the image urls of the top tracks
+def get_top_track_images(auth_header, tracks):
+    lst = []
+    for item in tracks:
+        track_id = item[1]
+        lst.append(track_id)
+    track_ids = ','.join(lst)
+    images = get_track_images(auth_header, track_ids)
+    return images
+
+
+# GET the image urls of the top artists
+def get_top_artist_images(auth_header, artists):
+    lst = []
+    for item in artists:
+        artist_id = item[1]
+        lst.append(artist_id)
+    artist_ids = ','.join(lst)
+    images = get_artist_images(auth_header, artist_ids)
+    return images
+
+
+# GET the tracks from a playlist
+def get_tracks_from_playlist(auth_header, list_id, person_type):
+    endpoint = "{}/playlists/{}/tracks".format(base_url, list_id)
+    response = requests.get(endpoint, headers=auth_header)
+    data = json.loads(response.text)
+    datapoints = get_dataframe(auth_header, data, person_type)
+    return datapoints
+
+
+# Function that can be called by a route based on term length
+def display_top_tracks(term_length):
+    # Get the access token from its cookie and use it to access data
+    access_token = request.cookies.get('token')
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
+    top_tracks_data = get_top_tracks_data(auth_header, term_length, '30')
+    images = get_top_track_images(auth_header, top_tracks_data)
+    return (top_tracks_data, images)
+
+
+# Function that can be called by a route based on term length
+def display_top_artists(term_length):
+    # Get the access token from its cookie and use it to access data
+    access_token = request.cookies.get('token')
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
+    top_artist_data = get_top_artist_data(auth_header, term_length, '30')
+    images = get_top_artist_images(auth_header, top_artist_data)
+    return (top_artist_data, images)
+
+
+# Function that can be called by a route based on term length
+def display_top_tracks_by_artist(term_length):
+    # Get the access token from its cookie and use it to access data
+    access_token = request.cookies.get('token')
+    auth_header = {"Authorization": "Bearer {}".format(access_token)}
+    data = get_top_tracks_by_artist(auth_header)
+    lst = []
+    for item in data:
+        lst.append(item)
+    images = get_top_artist_images(auth_header, lst)
+    return (data, images)
+
+
 # GET audio features for several tracks and store necessary datapoints
 def do_audio_analysis(auth_header, track_ids):
     endpoint = "{}/audio-features?ids={}".format(base_url, track_ids)
@@ -166,7 +207,7 @@ def make_graph(datapoints, tag):
     title = y_title + " ratings of your most recent songs"
     sns_plot = sns.barplot(x="Recent Songs", y=y_title, data=df).set_title(title)
     fig = sns_plot.get_figure()
-    fig.set_size_inches(15, 7.5)     # Should change this to relative size of the screen
+    fig.set_size_inches(15, 7.5)   
     fig.savefig('static/{t}.png'.format(t=tag))
 
 
@@ -194,7 +235,7 @@ def get_dataframe(auth_header, data, label):
     return df
 
 
-# 
+# Creates a radar chart
 def make_radar_chart():
     df = pd.DataFrame({
     'group': ['A','B','C','D'],
@@ -218,52 +259,10 @@ def make_radar_chart():
     # plt.ylim(0,40)
     # ax.plot(angles, values, linewidth=1, linestyle='solid')
     # ax.fill(angles, values, 'b', alpha=0.1)
-    #fig.savefig("/static/radar.png")
+    # fig.savefig("/static/radar.png")
 
 
-# GET the tracks from a playlist
-def get_tracks_from_playlist(auth_header, list_id, person_type):
-    endpoint = "{}/playlists/{}/tracks".format(base_url, list_id)
-    response = requests.get(endpoint, headers=auth_header)
-    data = json.loads(response.text)
-    datapoints = get_dataframe(auth_header, data, person_type)
-    return datapoints
-
-
-# Initial route for user authentication with Spotify
-@app.route("/")
-def index():
-    # Redirects the user to the Spotify login page (first thing that happens upon app launch)
-    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query.items()])
-    authorization = "{}/?{}".format(auth_url, url_args)
-    return redirect(authorization)
-
-
-# Homepage of application
-@app.route("/welcome")
-def display_top_data():
-    access_token = request.cookies.get('token')
-    if access_token is None:
-        access_token = generate_access_token()
-    # Obtain an access token either by generating a new one or retrieving from storage
-    # f = open("token.txt", "r")
-    # if os.stat("token.txt").st_size == 0:
-    #     access_token = generate_access_token()
-    # else:
-    #     access_token = f.readline()
-    # Use the token to get the necessary authorization header and access data
-    auth_header = {"Authorization": "Bearer {}".format(access_token)}
-    recent_tracks_data = get_recent_tracks_data(auth_header, '50')
-    recent_track_ids = get_recent_tracks_ids(auth_header, '50')
-    track_images = get_track_images(auth_header, recent_track_ids)
-
-    response = make_response(render_template("index.html", recent=recent_tracks_data, images=track_images))
-    response.set_cookie('token', access_token, max_age=3600)
-    return response
-    # Render HTML with the desired data
-    # return render_template("index.html", recent=recent_tracks_data, images=track_images)
-
-
+# Return predictions from the ML model
 def model_predict(datapoints):
     df = pd.DataFrame()
     df['Danceability'] = datapoints['danceability']
@@ -279,69 +278,34 @@ def model_predict(datapoints):
     predictions = xgb_loaded.predict(df)
     return predictions
 
-    
-# Function that returns the image urls of the top tracks
-def get_top_track_images(auth_header, tracks):
-    lst = []
-    for item in tracks:
-        track_id = item[1]
-        lst.append(track_id)
-    track_ids = ','.join(lst)
-    images = get_track_images(auth_header, track_ids)
-    return images
+
+# Initial route for user authentication with Spotify
+@app.route("/")
+def index():
+    # Redirects the user to the Spotify login page (first thing that happens upon app launch)
+    url_args = "&".join(["{}={}".format(key, quote(val)) for key, val in auth_query.items()])
+    authorization = "{}/?{}".format(auth_url, url_args)
+    return redirect(authorization)
 
 
-# Function that returns the image urls of the top artists
-def get_top_artist_images(auth_header, artists):
-    lst = []
-    for item in artists:
-        artist_id = item[1]
-        lst.append(artist_id)
-    artist_ids = ','.join(lst)
-    images = get_artist_images(auth_header, artist_ids)
-    return images
+# Homepage of application
+@app.route("/welcome")
+def display_top_data():
+    # Get an access token from its cookie or generate one if it doesn't exist yet
+    access_token = request.cookies.get('token')
+    if access_token is None:
+        access_token = generate_access_token()
 
-
-# Function that can be called by a route based on term length
-def display_top_tracks(term_length):
-    # Obtain the access token from where it is stored
-    f = open("token.txt", "r")
-    access_token = f.readline()
-    
     # Use the token to get the necessary authorization header and access data
     auth_header = {"Authorization": "Bearer {}".format(access_token)}
-    top_tracks_data = get_top_tracks_data(auth_header, term_length, '30')
-    images = get_top_track_images(auth_header, top_tracks_data)
-    return (top_tracks_data, images)
+    recent_tracks_data = get_recent_tracks_data(auth_header, '50')
+    recent_track_ids = get_recent_tracks_ids(auth_header, '50')
+    track_images = get_track_images(auth_header, recent_track_ids)
 
-
-# Function that can be called by a route based on term length
-def display_top_artists(term_length):
-    # Obtain the access token from where it is stored
-    f = open("token.txt", "r")
-    access_token = f.readline()
-    
-    # Use the token to get the necessary authorization header and access data
-    auth_header = {"Authorization": "Bearer {}".format(access_token)}
-    top_artist_data = get_top_artist_data(auth_header, term_length, '30')
-    images = get_top_artist_images(auth_header, top_artist_data)
-    return (top_artist_data, images)
-
-
-# Function that can be called by a route based on term length
-def display_top_tracks_by_artist(term_length):
-    # Obtain the access token from where it is stored
-    f = open("token.txt", "r")
-    access_token = f.readline()
-    
-    # Use the token to get the necessary authorization header and access data
-    auth_header = {"Authorization": "Bearer {}".format(access_token)}
-    data = get_top_tracks_by_artist(auth_header)
-    lst = []
-    for item in data:
-        lst.append(item)
-    images = get_top_artist_images(auth_header, lst)
-    return (data, images)
+    # Store HTML rendering in a response and create a cookie for the access token
+    response = make_response(render_template("index.html", recent=recent_tracks_data, images=track_images))
+    response.set_cookie('token', access_token, max_age=3600, secure=True)
+    return response
 
 
 # Page for viewing top tracks in the past 1 month
@@ -402,11 +366,8 @@ def display_top_tracks_by_artist_short_term():
 # Page for viewing an audio analysis graphS
 @app.route("/audio-analysis")
 def audio_analysis():
-    # Obtain the access token from where it is stored
-    f = open("token.txt", "r")
-    access_token = f.readline()
-    
-    # Use the token to get the necessary authorization header and access data
+    # Get the access token from its cookie and use it to access data
+    access_token = request.cookies.get('token')
     auth_header = {"Authorization": "Bearer {}".format(access_token)}
     track_ids = get_recent_tracks_ids(auth_header, '50')
     
@@ -429,8 +390,8 @@ def audio_analysis():
 # Page for viewing a user's sentiment analysis
 @app.route("/personality-analysis")
 def predict_personality():
-    f = open("token.txt", "r")
-    access_token = f.readline()
+    access_token = request.cookies.get('token')
+
     auth_header = {"Authorization": "Bearer {}".format(access_token)}
     """
     3 Personality types instead of 5 
@@ -439,9 +400,12 @@ def predict_personality():
     Submissive/conformist/passive  Type C = 2
     """
     frame_list = []
-    outgoing_list = ['37i9dQZF1DX3rxVfibe1L0', '37i9dQZF1DX6GwdWRQMQpq','37i9dQZF1DXdVbxH0H5oTi','37i9dQZF1DXdPec7aLTmlC', '37i9dQZF1DWSf2RDTDayIx', '37i9dQZF1DX7KNKjOK0o75'] # 476 Songs
-    mellow_list = ['37i9dQZF1DX6ziVCJnEm59', '37i9dQZF1DWSiZVO2J6WeI', '37i9dQZF1DX4E3UdUs7fUx', '37i9dQZF1DWYiR2Uqcon0X', '37i9dQZF1DWUvQoIOFMFUT']#300 something
-    passive_list = ['37i9dQZF1DX3YSRoSdA634','37i9dQZF1DX7gIoKXt0gmx','37i9dQZF1DWX83CujKHHOn', '37i9dQZF1DWSqBruwoIXkA', '37i9dQZF1DWVrtsSlLKzro'] #413
+    # 476 songs
+    outgoing_list = ['37i9dQZF1DX3rxVfibe1L0', '37i9dQZF1DX6GwdWRQMQpq','37i9dQZF1DXdVbxH0H5oTi','37i9dQZF1DXdPec7aLTmlC', '37i9dQZF1DWSf2RDTDayIx', '37i9dQZF1DX7KNKjOK0o75']
+    # ~300 songs
+    mellow_list = ['37i9dQZF1DX6ziVCJnEm59', '37i9dQZF1DWSiZVO2J6WeI', '37i9dQZF1DX4E3UdUs7fUx', '37i9dQZF1DWYiR2Uqcon0X', '37i9dQZF1DWUvQoIOFMFUT']
+    # 413 songs
+    passive_list = ['37i9dQZF1DX3YSRoSdA634','37i9dQZF1DX7gIoKXt0gmx','37i9dQZF1DWX83CujKHHOn', '37i9dQZF1DWSqBruwoIXkA', '37i9dQZF1DWVrtsSlLKzro']
 
     for item in passive_list:
         frame_list.append(get_tracks_from_playlist(auth_header, item, 2))
@@ -454,7 +418,6 @@ def predict_personality():
     track_ids = get_recent_tracks_ids(auth_header, '50')
     datapoints = do_audio_analysis(auth_header, track_ids)
     predictions = model_predict(datapoints)
-    print(len(predictions))
     make_radar_chart()
     return render_template("person.html")
 
